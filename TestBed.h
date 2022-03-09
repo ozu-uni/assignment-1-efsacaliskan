4edefe1f77df1f769ac0d5293ad15d5803720142
efsacaliskan <100141705+efsacaliskan@users.noreply.github.com>
2022-03-09 13:17:31 +0300

    Update TestBed.h

--- TestBed.h
+++ TestBed.h
@@ -1,1 +1,19 @@
-// write your codes here
+#ifndef TESTBED
+#define TESTBED
+#include "SelectionAlgorithm.h"
+#include "AlgorithmSortAll.h"
+#include "AlgorithmSortK.h"
+
+class TestBed {
+
+private:
+    SelectionAlgorithm* algorithm;
+
+public:
+    void execute();
+    void setAlgorithm(int, int);
+    TestBed();
+    ~TestBed();
+};
+
+#endif 
