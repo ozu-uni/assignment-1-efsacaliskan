0eec3944c9e0acf0e19ab2f935b41c6668eb58f4
efsacaliskan <100141705+efsacaliskan@users.noreply.github.com>
2022-03-09 13:12:33 +0300

    Update AlgorithmSortAll.h

--- AlgorithmSortAll.h
+++ AlgorithmSortAll.h
@@ -1,1 +1,16 @@
-// write your codes here
+#ifndef ALGORITHMSORTALL
+#define ALGORITHMSORTALL
+#include "SelectionAlgorithm.h"
+#include <iostream>
+
+using namespace std;
+
+class AlgorithmSortAll :public SelectionAlgorithm {
+public:
+    AlgorithmSortAll(int k);
+    virtual int select();
+
+};
+
+
+#endif 
