be708d425f611745bf703fa9e2fd6bee44dc3453
efsacaliskan <100141705+efsacaliskan@users.noreply.github.com>
2022-03-09 13:17:09 +0300

    Update TestBed.cpp

--- TestBed.cpp
+++ TestBed.cpp
@@ -1,1 +1,34 @@
-// write your codes here
+#include "TestBed.h"
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+void TestBed::execute() {
+
+    clock_t start = clock();
+    algorithm->select();
+    clock_t end = clock();
+    double cpu_time = static_cast<double>(end - start) / CLOCKS_PER_SEC;
+
+    cout << "Duration(sec):" << cpu_time << endl;
+
+}
+void TestBed::setAlgorithm(int type, int k) {
+
+    if (type == 1) {
+        algorithm = new AlgorithmSortAll(k);
+    }
+    else if (type == 2) {
+        algorithm = new AlgorithmSortK(k);
+    }
+
+};
+
+TestBed::TestBed() {
+    this->algorithm = NULL;
+}
+
+TestBed::~TestBed() {
+    delete[] algorithm;
+}
