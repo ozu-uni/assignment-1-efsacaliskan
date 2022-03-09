473223828dd14110015ccd9080ff889c8b7e5dd1
efsacaliskan <100141705+efsacaliskan@users.noreply.github.com>
2022-03-09 13:11:49 +0300

    Update AlgorithmSortAll.cpp

--- AlgorithmSortAll.cpp
+++ AlgorithmSortAll.cpp
@@ -1,1 +1,44 @@
-// write your codes here
+#include "AlgorithmSortAll.h"
+
+AlgorithmSortAll::AlgorithmSortAll(int k) : SelectionAlgorithm(k) {
+    this->k = k;
+}
+
+int AlgorithmSortAll::select() {
+
+    int N = 0;
+    int x = 0;
+    int* pNums = 0; 
+    
+    cin >> N;
+ 
+    pNums = new int[N];
+    for (int i = 0; i < N; i++) {
+
+        cin >> x;
+        pNums[i] = x;
+
+    }
+    int* temp = 0;
+    
+    int  i, a, j;
+    for (i = 1; i < N; i++) {
+        a = pNums[i];
+        j = i - 1;
+        while (j >= 0 && pNums[j] < a) {
+            pNums[j + 1] = pNums[j];
+            j = j - 1;
+        }
+        pNums[j + 1] = a;
+    }
+
+    cout << "Result: " << pNums[k - 1] << endl;
+    int result = pNums[k - 1];
+
+    delete[] pNums; 
+    pNums = 0;
+
+    return result;
+
+
+}
