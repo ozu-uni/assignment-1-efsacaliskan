60e37178bdccd13b3b61f4c235ad1d1ccd92c87c
efsacaliskan <100141705+efsacaliskan@users.noreply.github.com>
2022-03-09 13:14:51 +0300

    Update AlgorithmSortK.h

--- AlgorithmSortK.h
+++ AlgorithmSortK.h
@@ -1,1 +1,18 @@
-// write your codes here
+#ifndef ALGORITHMSORTK
+#define ALGORITHMSORTK
+#include "SelectionAlgorithm.h"
+#include <iostream>
+
+using namespace std;
+
+class AlgorithmSortK :public SelectionAlgorithm {
+
+public:
+    AlgorithmSortK(int k);
+
+    virtual int select();
+
+};
+
+#endif
+
