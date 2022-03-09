82ca8dcb9b38952b44268802e02fde165be4855e
efsacaliskan <100141705+efsacaliskan@users.noreply.github.com>
2022-03-09 13:16:06 +0300

    Update SelectionAlgorithm.cpp

--- SelectionAlgorithm.cpp
+++ SelectionAlgorithm.cpp
@@ -1,1 +1,12 @@
-// write your codes here
+#include "SelectionAlgorithm.h"
+#include <iostream>
+
+using namespace std;
+
+SelectionAlgorithm::SelectionAlgorithm(int k) {
+    this->k = k;
+}
+
+int SelectionAlgorithm::select() {
+    return 0;
+}
